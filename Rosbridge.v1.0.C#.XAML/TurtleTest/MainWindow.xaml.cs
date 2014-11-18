6884a40c26b288c9d11ae706a83fc0979135e44d
horverno@gmail.com <horverno@gmail.com@cb3a5c90-e9f6-85e4-40f7-8c022fe8bb61>
2014-11-18 17:22:15 +0000

    Initial structure of RosbridgeXAML solution added.

---
+++ Rosbridge.v1.0.C#.XAML/TurtleTest/MainWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TurtleTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Socket _clientSocket;
+        public MainWindow()
+        {
+            InitializeComponent();
+            stackControls.Visibility = System.Windows.Visibility.Hidden;
+        }
+
+        private void btnConnect_Click(object sender, RoutedEventArgs e)
+        {
+            if (btnConnect.Content.Equals("Connect"))
+            {
+                btnConnect.Background = Brushes.LightGreen;
+                btnConnect.Content = "Disonnect";
+                stackControls.Visibility = System.Windows.Visibility.Visible;
+                ConnectToRos();
+            }
+            else
+            {
+                btnConnect.Background = Brushes.OrangeRed;
+                btnConnect.Content = "Connect";
+                stackControls.Visibility = System.Windows.Visibility.Hidden;
+            }
+        }
+
+        private void btnSubscribe_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnForward_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnBackward_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnLeft_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnRight_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        void ConnectToRos()
+        {
+            // See if we have text on the IP and Port text fields
+            if (txtIP.Text == "" || txtPort.Text == "")
+            {
+                MessageBox.Show("IP Address and Port Number are required to connect to the Server\n");
+                return;
+            }
+            try
+            {
+                _clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                // Cet the remote IP address
+                IPAddress ip = IPAddress.Parse(txtIP.Text);
+                int iPortNo = System.Convert.ToInt16(txtPort.Text);
+                // Create the end point 
+                IPEndPoint ipEnd = new IPEndPoint(ip, iPortNo);
+                // Connect to the remote host
+                _clientSocket.Connect(ipEnd);
+                if (_clientSocket.Connected)
+                {
+                    //Wait for data asynchronously 
+                    WaitForData();
+                }
+                _clientSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+                byte[] handShake = Encoding.UTF8.GetBytes("raw\r\n\r\n");
+                _clientSocket.Send(handShake);
+            }
+            catch (SocketException se)
+            {
+                string str;
+                str = "\nConnection failed, is the server running?\n" + se.Message;
+                MessageBox.Show(str);
+            }
+        }
+        void DisconnectFromRos()
+        {
+            if (_clientSocket != null)
+            {
+                _clientSocket.Close();
+                _clientSocket = null;
+            }
+        }
+    }
+}
