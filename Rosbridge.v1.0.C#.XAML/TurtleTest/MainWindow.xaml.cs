7c2e098da24624bd2eaeaa7312d3a9233d2f7787
horverno@gmail.com <horverno@gmail.com@cb3a5c90-e9f6-85e4-40f7-8c022fe8bb61>
2014-11-19 14:24:16 +0000

    [UnStable]

--- Rosbridge.v1.0.C#.XAML/TurtleTest/MainWindow.xaml.cs
+++ Rosbridge.v1.0.C#.XAML/TurtleTest/MainWindow.xaml.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +25,14 @@ namespace TurtleTest
     public partial class MainWindow : Window
     {
         private Socket _clientSocket;
+        private AsyncCallback _pfnCallBack;
+        private IAsyncResult _result;
+        public String _pureResponse = "";
+        public class SocketPacket
+        {
+            public System.Net.Sockets.Socket thisSocket;
+            public byte[] dataBuffer = new byte[1];
+        }
         public MainWindow()
         {
             InitializeComponent();
@@ -33,13 +43,17 @@ namespace TurtleTest
         {
             if (btnConnect.Content.Equals("Connect"))
             {
-                btnConnect.Background = Brushes.LightGreen;
-                btnConnect.Content = "Disonnect";
-                stackControls.Visibility = System.Windows.Visibility.Visible;
-                ConnectToRos();
+                if (ConnectToRos())
+                {
+                    btnConnect.Background = Brushes.LightGreen;
+                    btnConnect.Content = "Disonnect";
+                    stackControls.Visibility = System.Windows.Visibility.Visible;
+                }
             }
             else
             {
+
+                DisconnectFromRos();
                 btnConnect.Background = Brushes.OrangeRed;
                 btnConnect.Content = "Connect";
                 stackControls.Visibility = System.Windows.Visibility.Hidden;
@@ -48,35 +62,65 @@ namespace TurtleTest
 
         private void btnSubscribe_Click(object sender, RoutedEventArgs e)
         {
+           RosBridgeDotNet.RosBridgeDotNet.SubscribeMessage rosSubscMsg;
+           try
+           {
+               object[] poseSubscribe = { "/turtle1/pose", 500 };
+               if (btnSubscribe.Content.Equals("Subcribe"))
+               {
+                   rosSubscMsg = new RosBridgeDotNet.RosBridgeDotNet.SubscribeMessage("/rosbridge/subscribe", poseSubscribe);
+               }
+               else
+               {
+                   rosSubscMsg = new RosBridgeDotNet.RosBridgeDotNet.SubscribeMessage("/rosbridge/unsubscribe", poseSubscribe);
+               }
+               string needToSend = JsonConvert.SerializeObject(u);
+               //Object objData = SerializeEventData(new EventData(richTextTxName.Text));
+               //byte[] byData = System.Text.Encoding.UTF8.GetBytes(objData.ToString());
+               if (_clientSocket != null)
+               {
+                   _clientSocket.Send(new byte[] { 0 });    // \x00
+                   _clientSocket.Send(Encoding.UTF8.GetBytes(needToSend));
+                   _clientSocket.Send(new byte[] { 255 });    // \xff
+               }
+           }
+           catch (SocketException se)
+           {
+               MessageBox.Show(se.Message);
+           }
+       }
 
-        }
 
         private void btnForward_Click(object sender, RoutedEventArgs e)
         {
-
+            PublishturtleMessage(1, 0);
         }
 
         private void btnBackward_Click(object sender, RoutedEventArgs e)
         {
-
+            PublishturtleMessage(-1, 0);
         }
 
         private void btnLeft_Click(object sender, RoutedEventArgs e)
         {
-
+            PublishturtleMessage(0, 1);
         }
 
         private void btnRight_Click(object sender, RoutedEventArgs e)
         {
-
+            PublishturtleMessage(0, -1);
         }
-        void ConnectToRos()
+        /// <summary>
+        /// Return true if connection was succesful
+        /// </summary>
+        /// <returns></returns>
+        bool ConnectToRos()
         {
             // See if we have text on the IP and Port text fields
             if (txtIP.Text == "" || txtPort.Text == "")
             {
                 MessageBox.Show("IP Address and Port Number are required to connect to the Server\n");
-                return;
+                return false;
             }
             try
             {
@@ -96,12 +140,14 @@ namespace TurtleTest
                 _clientSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
                 byte[] handShake = Encoding.UTF8.GetBytes("raw\r\n\r\n");
                 _clientSocket.Send(handShake);
+                return true;
             }
             catch (SocketException se)
             {
                 string str;
                 str = "\nConnection failed, is the server running?\n" + se.Message;
                 MessageBox.Show(str);
+                return false;
             }
         }
         void DisconnectFromRos()
@@ -110,6 +156,104 @@ namespace TurtleTest
             {
                 _clientSocket.Close();
                 _clientSocket = null;
+            }
+        }
+        public void WaitForData()
+        {
+            try
+            {
+                if (_pfnCallBack == null)
+                {
+                    _pfnCallBack = new AsyncCallback(OnDataReceived);
+                }
+                SocketPacket theSocPkt = new SocketPacket();
+                theSocPkt.thisSocket = _clientSocket;
+                // Start listening to the data asynchronously
+                _result = _clientSocket.BeginReceive(theSocPkt.dataBuffer,
+                                                        0, theSocPkt.dataBuffer.Length,
+                                                        SocketFlags.None,
+                                                        _pfnCallBack,
+                                                        theSocPkt);
+            }
+            catch (SocketException se)
+            {
+                MessageBox.Show(se.Message);
+            }
+            catch (NullReferenceException ne)
+            {
+                MessageBox.Show(ne.Message);
+            }
+
+        }
+        public void OnDataReceived(IAsyncResult asyn)
+        {
+            try
+            {
+                SocketPacket theSockId = (SocketPacket)asyn.AsyncState;
+                int iRx = theSockId.thisSocket.EndReceive(asyn);
+                char[] chars = new char[iRx + 1];
+                System.Text.Decoder d = System.Text.Encoding.UTF8.GetDecoder();
+                int charLen = d.GetChars(theSockId.dataBuffer, 0, iRx, chars, 0);
+                System.String szData = new System.String(chars);
+                try
+                {
+                    //RetrieveReceivedEventData(pureResponse);
+                    szData = Regex.Replace(szData, @"[^\u0000-\u007F]", string.Empty);
+                    System.Diagnostics.Debug.WriteLine(szData.ToString() + " ");
+                    //richTextRxMessage.Text = richTextRxMessage.Text + "a";
+
+                }
+                catch (Exception e)
+                {
+                    //MessageBox.Show(e.ToString());
+                }
+
+                //If we finished receiving the response (EventData object) from the Broker
+                //then we will recover it
+                /*if (richTextRxMessage.Text.Contains("ENDOFMESSAGE"))
+                {
+                    //Remove the flag "ENDOFMESSAGE"
+                    String pureResponse = richTextRxMessage.Text
+                        .Remove(richTextRxMessage.Text.IndexOf("ENDOFMESSAGE"), 12);
+                    RetrieveReceivedEventData(pureResponse);
+
+                    richTextBoxHistoryResponses.Text += "\n \\\\\\\\\\\\ \n Response : " + pureResponse;
+                    richTextRxMessage.Text = "";
+                }*/
+
+                WaitForData();
+            }
+            catch (ObjectDisposedException)
+            {
+                System.Diagnostics.Debugger.Log(0, "1", "\nOnDataReceived: Socket has been closed\n");
+            }
+            catch (SocketException se)
+            {
+                MessageBox.Show(se.Message);
+            }
+        }
+
+        private void PublishturtleMessage(double linear, double angular)
+        {
+            try
+            {
+                string topic = "/turtle1/command_velocity";
+                string msgtype = "turtlesim/Velocity";
+                RosBridgeDotNet.RosBridgeDotNet.TurtleSim turtleGo1 = new RosBridgeDotNet.RosBridgeDotNet.TurtleSim(linear, angular);
+                RosBridgeDotNet.RosBridgeDotNet.PublishMessage m = new RosBridgeDotNet.RosBridgeDotNet.PublishMessage(topic, msgtype, turtleGo1);
+                string needToSend = JsonConvert.SerializeObject(m);
+                //Object objData = SerializeEventData(new EventData(richTextTxName.Text));
+                //byte[] byData = System.Text.Encoding.UTF8.GetBytes(objData.ToString());
+                if (_clientSocket != null)
+                {
+                    _clientSocket.Send(new byte[] { 0 });    // \x00
+                    _clientSocket.Send(Encoding.UTF8.GetBytes(needToSend));
+                    _clientSocket.Send(new byte[] { 255 });    // \xff
+                }
+            }
+            catch (SocketException se)
+            {
+                MessageBox.Show(se.Message);
             }
         }
     }
